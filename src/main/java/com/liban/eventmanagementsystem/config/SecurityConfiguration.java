f171982400ed0cf778266a2a420c278da13854ed
LibanAbdullahiM <libanr4243@gmail.com>
2026-04-03 18:38:55 +0300

    Implement InMemory-based authentication with UserDetailsService. closes #7

--- src/main/java/com/liban/eventmanagementsystem/config/SecurityConfiguration.java
+++ src/main/java/com/liban/eventmanagementsystem/config/SecurityConfiguration.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,5 +32,26 @@ public class SecurityConfiguration {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        PasswordEncoder encoder = PasswordEncoderFactories
+                .createDelegatingPasswordEncoder();
+
+        UserDetails user1 = User
+                .withUsername("liban")
+                .password(encoder.encode("l@123"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user2 = User
+                .withUsername("safio")
+                .password(encoder.encode("s@123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
