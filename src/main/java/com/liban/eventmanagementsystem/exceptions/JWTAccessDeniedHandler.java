b71fbafe7cfd40a30c09e2bc655e233d8472dd9b
LibanAbdullahiM <libanr4243@gmail.com>
2026-04-19 14:59:58 +0300

    Fixed the vulnerabilities of these dependencies: 1 jackson-core 2 tomcat-embed-core used their latest versions.

--- src/main/java/com/liban/eventmanagementsystem/exceptions/JWTAccessDeniedHandler.java
+++ src/main/java/com/liban/eventmanagementsystem/exceptions/JWTAccessDeniedHandler.java
@@ -1,12 +1,13 @@
 package com.liban.eventmanagementsystem.exceptions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
+
 
 import java.io.IOException;
 import java.time.LocalDateTime;
--- src/main/java/com/liban/eventmanagementsystem/exceptions/JWTAuthenticationEntryPoint.java
+++ src/main/java/com/liban/eventmanagementsystem/exceptions/JWTAuthenticationEntryPoint.java
@@ -1,12 +1,12 @@
 package com.liban.eventmanagementsystem.exceptions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
